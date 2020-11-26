781ad3f4b78bb7e5d0fe321f02419e8bad70c242
yxu027 <yxu027@ucr.edu>
2020-11-25 22:16:10 -0800

    plot2

--- plot2.R
+++ plot2.R
@@ -4,8 +4,16 @@ names <- read.table(unz("exdata_data_household_power_consumption.zip", "househol
 data <- read.table(text = grep("^[1,2]/2/2007",readLines(unz("exdata_data_household_power_consumption.zip", "household_power_consumption.txt")),value=TRUE), sep = ';', header=T, col.names = names)
 
 
-#Plot 1
+#convert date to weekday
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+# data$weekday <- weekdays(data$Date, abbreviate = TRUE)
+
+#Plot 2
 if(dev.cur() == 1) dev.new()
-hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
-dev.copy(png,"plot1.png", width = 480, height = 480, units='px')
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = "Global Active Power (kilowatt)", type = "l")
+dev.copy(png,"plot2.png", width = 480, height = 480, units='px')
 dev.off()
+
+
+
