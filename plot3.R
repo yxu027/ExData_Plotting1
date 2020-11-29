b08b57f117ed0da6ecee311c9bb787623ade03c7
yxu027 <yxu027@ucr.edu>
2020-11-28 22:26:09 -0800

    minor changes

--- plot3.R
+++ plot3.R
@@ -14,3 +14,7 @@ lines(data$DateTime, data$Sub_metering_2, xlab = '', type = "l", col = "red")
 lines(data$DateTime, data$Sub_metering_3, xlab = '', type = "l", col = "blue")
 
 legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ), lwd = "1")
+
+dev.copy(png,"plot3.png", width = 480, height = 480, units='px')
+dev.off()
+
